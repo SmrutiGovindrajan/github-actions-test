4f7c07017009b39280af0db28d66512ed13162ba
Smruti Govindrajan <smruti12june@gmail.com>
2024-11-24 15:50:31 -0700

    Initial Setup

---
+++ scripts/deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# check if gh binary is available
+if [ -x "$(command -v gh)" ]; then
+    echo 'gh cli is installed.'
+    exit 0
+fi
+
+echo 'Installing gh cli'
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
